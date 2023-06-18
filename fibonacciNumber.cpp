6e2d4cbbf825d0800c09f1983a5bf08495553ada
Kritesh <97170144+itskrv@users.noreply.github.com>
2023-06-18 17:16:13 +0530

    Update fibonacciNumber.cpp

--- fibonacciNumber.cpp
+++ fibonacciNumber.cpp
@@ -1,3 +1,4 @@
+// using recursion 
 class Solution {
 public:
     int fib(int n) {
@@ -9,5 +10,29 @@ public:
         return 1;
 
         return fib(n-1)+fib(n-2);
+    }
+};
+// By using for loop
+class Solution {
+public:
+    int fib(int n) {
+        //Base condition
+        if(n==0)
+        return 0;
+
+        if(n==1)
+        return 1;
+
+         int a = 0, b = 1, ans = 0;
+
+         for(int i=1; i<n; i++){
+
+             ans = a+b;
+             a = b;
+             b = ans;
+         }
+
+         return ans;
+
     }
 };
