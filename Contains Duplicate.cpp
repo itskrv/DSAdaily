6ab28e033eb90ea7d998199284098070cc8b80c0
Kritesh <97170144+itskrv@users.noreply.github.com>
2023-10-02 08:32:06 +0530

    Create Contains Duplicate.cpp

---
+++ Contains Duplicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+      unordered_set<int> myset;
+
+ 
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        
+        if (myset.find(nums[i]) != myset.end())
+            return true;
+ 
+    
+        myset.insert(nums[i]);
+ 
+     
+    }
+    return false;
+        
+    }
+};
